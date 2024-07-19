4ed84e91c99e302c234fca2cd5971c14db817712
Blessonpx <36164387+Blessonpx@users.noreply.github.com>
2024-07-19 16:38:14 +0000

    Initial commit

---
+++ src/main/java/ThreadTest.java
@@ -0,0 +1,75 @@
+
+
+public class ThreadTest {
+	public static final int DELAY=10;
+	public static final int STEPS=100;
+	public static final double MAX_AMOUNT=1000;
+	
+	public static void main(String[] args) {
+		Bank bank = new Bank(4,100000);
+	
+		Runnable task1 = () ->
+		{
+			try {
+				for(int i=0; i<STEPS; i++) {
+					double amount = MAX_AMOUNT*Math.random();
+					bank.transfer(0,1,amount);
+					Thread.sleep((int) (DELAY* Math.random()));
+				}
+			}catch(InterruptedException e) {
+				
+			}
+		};
+		
+		Runnable task2 = () ->
+		{
+			try {
+				for(int i=0; i<STEPS;i++) {
+					double amount = MAX_AMOUNT*Math.random();
+					bank.transfer(2,3,amount);
+					Thread.sleep((int) (DELAY* Math.random()));
+				}
+			}catch(InterruptedException e) {
+				
+			}
+		};
+
+		// Following task shows what a race condition might be 
+
+		Runnable r1 = () ->
+			{
+				try {
+					while(true) {
+						int toAccount = (int) (Math.random()*bank.size());
+						int fromAccount = (int) (Math.random()*bank.size());
+						double amount = MAX_AMOUNT*Math.random();
+						bank.transfer(fromAccount,toAccount,amount);
+						Thread.sleep((int) (DELAY* Math.random()));
+					}
+				}catch(InterruptedException e) {
+
+				}
+			};
+
+		Runnable r2 = () ->
+			{
+				try {
+					while(true) {
+						int toAccount = (int) (Math.random()*bank.size());
+						int fromAccount = (int) (Math.random()*bank.size());
+						double amount = MAX_AMOUNT*Math.random();
+						bank.transfer(fromAccount,toAccount,amount);
+						Thread.sleep((int) (DELAY* Math.random()));
+					}
+				}catch(InterruptedException e) {
+
+				}
+			};
+		
+		//new Thread(task1).start();
+		//new Thread(task2).start();
+		new Thread(r1).start();
+		new Thread(r2).start();
+	}
+
+}
