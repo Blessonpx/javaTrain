69b6cd58e77aae17626e6d09317ccbf78b3ee32b
Blessonpx <36164387+Blessonpx@users.noreply.github.com>
2024-07-21 07:29:02 +0000

    Java Lock and Condition code

--- src/main/java/synch/SynchBankTest.java
+++ src/main/java/synch/SynchBankTest.java
@@ -1,4 +1,4 @@
-public class UnsynchBankTest {
+public class SynchBankTest {
   public static final int NACCOUNTS = 100;
   public static final double INITIAL_BALANCE = 1000;
   public static final double MAX_AMOUNT = 1000;
